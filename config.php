1767292d3a0be14182aa728fcd1d3e7cd25b1692
ZiadNady <94768692+ZiadNady@users.noreply.github.com>
2022-12-07 23:13:38 +0200

    done

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php 
+session_start();
+
+// base file link 
+define("BL",__DIR__.'/');
+define("BLA",__DIR__.'/admin/');
+
+// base url
+define("BU","http://localhost:8081/medical-project/");
+define("BUA","http://localhost:8081/medical-project/admin/");
+define("ASSETS","http://localhost:8081/medical-project/assets/");
+
+require BL.'functions/db.php'; 
+
+
+
+
+?>
+
